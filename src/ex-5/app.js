5c75ba37576dd22046639796003bf39139b884d2
Aitor Garcia Fernandez <aitorgarciafernandezcv@gmail.com>
2024-06-28 16:39:29 +0200

    ex5 done

---
+++ src/ex-5/app.js
@@ -0,0 +1,25 @@
+const num1 = document.getElementById("number-1");
+const num2 = document.getElementById("number-2");
+const btnGreater = document.getElementById("btn-greater");
+const mainBlock = document.querySelector("main");
+
+function render(msg) {
+  mainBlock.innerHTML = msg;
+}
+
+function greater(x, y) {
+  if (x > y) {
+    return `Entre ${x} y ${y} el mayor es: ${x}`;
+  }
+  if (x < y) {
+    return `Entre ${x} y ${y} el mayor es: ${y}`;
+  }
+  return `Son iguales`;
+}
+
+btnGreater.addEventListener("click", () => {
+  let n1 = num1.value;
+  let n2 = num2.value;
+
+  render(greater(parseInt(n1), parseInt(n2)));
+});
