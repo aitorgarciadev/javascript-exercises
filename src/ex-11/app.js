ba6ced8f45cf2cde5bfb7fcc50dc3227880709b0
Aitor Garcia Fernandez <aitorgarciafernandezcv@gmail.com>
2024-06-28 17:04:01 +0200

    until ex-11 done

---
+++ src/ex-11/app.js
@@ -0,0 +1,18 @@
+const mainBlock = document.querySelector("main");
+const number1 = document.getElementById("num-1").value;
+const btnCheck = document.getElementById("btn-check");
+
+function render(msg) {
+  mainBlock.innerHTML = msg;
+}
+
+function itsDivisible(num) {
+  if (num % 2 === 0 || num % 3 === 0 || num % 5 === 0 || num % 7 === 0) {
+    return `${num} es divisible por 2, 3, 5 o 7`;
+  }
+  return `${num} NO es divisible por 2, 3, 5 o 7`;
+}
+
+btnCheck.addEventListener("click", () => {
+  render(itsDivisible(number1));
+});
